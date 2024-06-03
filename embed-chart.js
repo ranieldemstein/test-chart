759f5f48b2ff67081ac2d634044257076c7cfea8
ranieldemstein <113078131+ranieldemstein@users.noreply.github.com>
2024-06-02 20:50:53 -0400

    Create embed-chart.js

---
+++ embed-chart.js
@@ -0,0 +1,15 @@
+(function() {
+  var container = document.createElement('div');
+  container.style.height = '100%';
+  container.style.width = '100%';
+  container.style.position = 'relative';
+
+  var iframe = document.createElement('iframe');
+  iframe.style.border = 'none';
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.src = 'https://raw.githack.com/ranieldemstein/test-chart/main/index.html?ticker=' + encodeURIComponent('AAPL'); // Change 'AAPL' to your default ticker
+
+  container.appendChild(iframe);
+  document.body.appendChild(container);
+})();
